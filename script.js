d5e103bddefb6fe8433dcb4ec4c82f8cd87be981
Matheus-Henrique-Burei <matheushburei.99@gmail.com>
2021-12-06 14:00:19 -0300

    adding images

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const initGame = () => {
             height: 8,
             size: 8,
             pixelsPerFields: 50,
+            coordenates: ['a','b','c','d','e','f','g','h'],
             light: "#f0d9b5",
             dark: "#b58863"
         }
@@ -17,7 +18,7 @@ const initGame = () => {
 }
 
 const drawBoard = (context, state) => {
-    const {width, height, size, pixelsPerFields, light, dark} = state.screen;
+    const {width, height, size, pixelsPerFields, coordenates, light, dark} = state.screen;
     let i = y = x = -1;
 
     context.clearRect(0,0, width * pixelsPerFields, height * pixelsPerFields);
@@ -27,10 +28,27 @@ const drawBoard = (context, state) => {
             y++
             x=0
         }
+
         context.beginPath();
         context.rect(x * pixelsPerFields, y * pixelsPerFields, pixelsPerFields, pixelsPerFields);
         context.fillStyle = (x + y) % 2 ? dark : light;
         context.fill();
+
+        if(i % size === 0) {
+            context.fillStyle = (x + y) % 2 ? light : dark;
+            context.fillText((i/size), (x+0.05) * pixelsPerFields, (y+0.2) * pixelsPerFields)
+        }
+        if(y+1 === size) {
+            context.fillStyle = (x + y) % 2 ? light : dark;
+            context.fillText(coordenates[x], (x+0.85) * pixelsPerFields, (y+0.9) * pixelsPerFields)
+            nx = x * pixelsPerFields
+            ny = y * pixelsPerFields
+            img = new Image()
+            img.src = 'assets/img/w_king.png'
+            context.drawImage( img, nx, ny, pixelsPerFields, pixelsPerFields)
+        }
+        
+
     }
 }
 
